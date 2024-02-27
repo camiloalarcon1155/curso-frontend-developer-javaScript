e5b8b3fbcfd25a8a3cadcbf726956dc67eaea0e7
Camilo Alarcón <camiloalarcon11155@gmail.com>
2024-02-27 08:57:19 -0500

    cambio de logo de carrito de compras

--- camkat.js
+++ camkat.js
@@ -370,7 +370,7 @@ function recargarMiPagina(){
 function ajustarCorreoSegunTamanioPantalla() {
     var screenWidth = window.innerWidth;
 
-    if (screenWidth <= 854) {
+    if (screenWidth <= 888) {
         /* logoEmailUsuario.innerHTML = ''; */
         logoEmailUsuario.innerHTML = '<div class = "divGmailUsuario"> <img class= "gmailUsuario" src="https://c0.klipartz.com/pngpicture/246/763/gratis-png-marca-angulo-del-corazon-gmail-logo-de-gmail-thumbnail.png" alt="Email"> </div>'
     } else {
